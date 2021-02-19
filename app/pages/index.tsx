75531e013f41ded10c15d0fa2e4c33c73b9ab2d3
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2021-02-19 17:33:30 +0100

    adds twitter hande

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -103,8 +103,20 @@ const Home: BlitzPage = () => {
       )}
 
       {showAccess && <AccessLog></AccessLog>}
-
-      <div className="text-2xl absolute z-10 top-8 left-0 w-full flex space-x-1 items-center p-2 ">
+      <div className="text-xl absolute z-10 top-9 right-2 text-white font-mono flex space-x-1 items-center p-2 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="#fff"
+          className="mr-2"
+        >
+          <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+        </svg>{" "}
+        @ntgussoni
+      </div>
+      <div className="text-2xl absolute z-10 top-8 left-2 w-full flex space-x-1 items-center p-2 ">
         <span className="font-bold mr-4">ENERGY</span>
         {energyMeter.map((i) => (
           <span
