b42d292519c15b2907120ef67706b0bf7f911ee8
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2021-02-20 10:39:10 +0100

    Adds seeds and updates readme

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,5 @@
 import db from "./index"
+import { SecurePassword } from "blitz"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -9,7 +10,34 @@ import db from "./index"
  */
 const seed = async () => {
   await db.reactor.deleteMany()
-  await db.reactor.create({ data: {} })
+  await db.user.deleteMany()
+
+  const hashedPassword = await SecurePassword.hash("password1234")
+
+  console.log("--- Creating reactor ----")
+  await db.reactor.create({ data: { generationMw: 1000 } })
+  console.log("--- Creating users ----")
+  await db.user.create({
+    data: { email: "director@example.com", role: "director", name: "Director", hashedPassword },
+  })
+  await db.user.create({
+    data: {
+      email: "technician@example.com",
+      role: "technician",
+      name: "Technician",
+      hashedPassword,
+    },
+  })
+  await db.user.create({
+    data: {
+      email: "secret-service@example.com",
+      role: "secret-service",
+      name: "Secret service",
+      hashedPassword,
+    },
+  })
+
+  console.log("--- All done ----")
 }
 
 export default seed
